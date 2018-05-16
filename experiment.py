fd564a69c32f176ff84c8c529f06b349a8d61db5
yoveravila93 <38230484+yoveravila93@users.noreply.github.com>
2018-05-16 11:13:22 -0500

    python
    cont

---
+++ experiment.py
@@ -0,0 +1,13 @@
+for number in range(5):
+      print number,
+
+d = { 
+  "name": "Eric",
+  "age": 26
+}
+
+for key in d:
+  print key, d[key],
+
+for letter in "Eric":
+  print letter,  # note the comma!
--- hello.py
+++ hello.py
@@ -1,4 +1,16 @@
-print "hello world"
-print "actualizacion"
-print "sera verdad"
-print "mi intento"
+# parte relevante
+    L1 = [1, 2, 3, 4, 5]
+    L2 = [4, 5, 6, 7, 8]
+
+    clean(L1, L2)
+
+    for i in range(max(len(L1), len(L2))):
+        a = getitem(L1, i, [])
+        b = getitem(L2, i, [])
+        diff = []
+
+        for j in range(max(len(a), len(b))):
+            if not getitem(a, j) == getitem(b, j):
+                diff.append(j)
+
+        print a, b, diff
